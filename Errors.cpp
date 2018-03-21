d9edc86963933f92e2c3df57b2b11f057ac6ca0e
remi <remi.verny@epitech.eu>
2018-03-21 20:43:43 +0100

    Added Errors.cpp and Errors.hpp. Created error classes with a few cases i could think of. More to be added in time.

---
+++ Errors.cpp
@@ -0,0 +1,59 @@
+//
+// EPITECH PROJECT, 2018
+// nanotekspice
+// File description:
+// Errors class
+//
+
+#include "Errors.hpp"
+
+ArcadeError::ArcadeError(std::string const &message, std::string const &component)
+	: _message(message), _component(component)
+{
+
+}
+
+ArcadeError::~ArcadeError()
+{
+
+}
+
+const std::string	&ArcadeError::getComponent() const noexcept
+{
+	return _message;
+}
+
+const char	*ArcadeError::what() const noexcept
+{
+	return _message.data();
+}
+
+ConfFileError::ConfFileError(std::string const &message, std::string const &component)
+	: ArcadeError(message, component)
+{
+
+}
+
+GraphicalInitError::GraphicalInitError(std::string const &message, std::string const &component)
+	: ArcadeError(message, component)
+{
+
+}
+
+GraphicalCleanupError::GraphicalCleanupError(std::string const &message, std::string const &component)
+	: ArcadeError(message, component)
+{
+
+}
+
+GameCleanupError::GameCleanupError(std::string const &message, std::string const &component)
+	: ArcadeError(message, component)
+{
+
+}
+
+GameInitError::GameInitError(std::string const &message, std::string const &component)
+	: ArcadeError(message, component)
+{
+
+}
---
+++ Errors.hpp
@@ -0,0 +1,69 @@
+//
+// EPITECH PROJECT, 2018
+// cpp_arcade
+// File description:
+// Errors class header
+//
+
+#ifndef ERRORS_HPP_
+# define ERRORS_HPP_
+
+# include <string>
+
+class   ArcadeError : public std::exception
+{
+public:
+        // Ctor
+        ArcadeError(std::string const &message,
+		 std::string const &component = "Unknown");
+
+        // Dtor
+        ~ArcadeError();
+
+	// Member functions
+	const std::string	&getComponent() const noexcept;
+	const char		*what() const noexcept override;
+
+protected:
+	// Properties
+
+	std::string	_message;
+	std::string	_component;
+};
+
+class	ConfFileError : public ArcadeError
+{
+public:
+	ConfFileError(std::string const &message,
+		  std::string const &component = "Unknown");
+};
+
+class   GraphicalInitError : public ArcadeError
+{
+public:
+        GraphicalInitError(std::string const &message,
+		  std::string const &component = "Unknown");
+};
+
+class   GraphicalCleanupError : public ArcadeError
+{
+public:
+        GraphicalCleanupError(std::string const &message,
+		  std::string const &component = "Unknown");
+};
+
+class   GameCleanupError : public ArcadeError
+{
+public:
+        GameCleanupError(std::string const &message,
+		  std::string const &component = "Unknown");
+};
+
+class   GameInitError : public ArcadeError
+{
+public:
+        GameInitError(std::string const &message,
+		  std::string const &component = "Unknown");
+};
+
+#endif /* ERRORS_HPP_ */
