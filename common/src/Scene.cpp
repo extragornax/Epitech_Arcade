e95bf079d701a15206d25c3b1b5d49ab3a55e326
Rémi Verny <remi.verny@epitech.eu>
2018-04-01 17:29:43 +0200

    Added scene.cpp

---
+++ common/src/Scene.cpp
@@ -0,0 +1,43 @@
+//
+// EPITECH PROJECT, 2018
+// cpp_arcade
+// File description:
+// Scene.cpp
+//
+
+#include "Scene.hpp"
+
+Scene::Scene() : _boardGame()
+{
+	
+}
+
+Scene::~Scene()
+{
+
+}
+
+Board	&Scene::getBoardGame()
+{
+	return _boardGame;
+}
+
+Board	&Scene::getBoardFooter()
+{
+	return _boardFooter;
+}
+
+std::string	&Scene::getTitle()
+{
+	return _title;
+}
+
+std::clock_t	&Scene::getClock()
+{
+	return _clock;
+}
+
+size_t	&Scene::getScore()
+{
+	return _score;
+}
