0cd462b0b7a83ff47c9fe5ba4fa08b7ca67adfc6
Extragornax <gaspardwi@yahoo.fr>
2018-04-01 15:15:21 +0200

    Patched pointers issues in core main

--- core/src/main.cpp
+++ core/src/main.cpp
@@ -12,15 +12,15 @@
 int	main(int ac, char **av)
 {
 	auto	load_graph = new Dlloader("./lib/lib_arcade_ncurses.so");
-        auto	load_game = new Dlloader("./game/lib_arcade_nibbler.so");
-	auto	lib_handler = load_graph.createLibSym();
-	auto	game_handler = load_game.createGameSym();
+	auto	load_game = new Dlloader("./game/lib_arcade_nibbler.so");
+	auto	lib_handler = load_graph->createLibSym();
+	auto	game_handler = load_game->createGameSym();
 
 	(void) ac;
 	(void) av;
-	while (lib_game.endGame() == false) {
-		lib_handler.drawScene(game_handler.updateScene(lib_handler.getKey()));
-		lib_handler.draw();
+	while (game_handler->endGame() == false) {
+		lib_handler->drawScene(game_handler.updateScene(lib_handler.getKey()));
+		lib_handler->draw();
 	}
 	return 0;
 }
