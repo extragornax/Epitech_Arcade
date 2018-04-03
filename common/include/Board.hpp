14d651883cf8c47b81e73e1fe789dc37f88db3d4
Extragornax <gaspardwi@yahoo.fr>
2018-04-03 14:52:56 +0200

    Updated board.cpp after meeting

--- common/include/Board.hpp
+++ common/include/Board.hpp
@@ -9,12 +9,11 @@
 
 #include <vector>
 #include <string>
-#include <array>
 #include "Utils.hpp"
 
 class Board {
 public:
-	Board();// = default;
+	Board() = default;
 	~Board() = default;
 
 public:
@@ -27,32 +26,42 @@ public:
 		void setSprites(const std::vector<std::string>);
 		void setCharacters(const std::vector<char>);
 		void setDirection(const Direction);
+		void setColorBackground(const Color);
+		void setColorForeground(const Color);
 
 	public:
 		std::vector<std::string> getSprites() const;
 		std::vector<char> getCharacters() const;
 		Direction getDirection() const;
+		Color getColorBackground() const;
+		Color getColorForeground() const;
 
 	private:
 		std::vector<std::string> _sprites;
 		std::vector<char> _characteres;
 		Direction _direction;
+		Color _background;
+		Color _foreground;
 	};
 
 public:
 	void setSprites(const Position &, const std::vector<std::string>);
 	void setCharacters(const Position &, const std::vector<char>);
 	void setDirection(const Position &, const Direction);
+	void setColorBackground(const Position &, const Color);
+	void setColorForeground(const Position &, const Color);
 
 public:
 	std::vector<std::string> getSprites(const Position &) const;
 	std::vector<char> getCharacters(const Position &) const;
 	Direction getDirection(const Position &) const;
+	Color getColorBackground(const Position &) const;
+	Color getColorForeground(const Position &) const;
 
 public:
 	void createTile(const Position &, const std::vector<std::string>,
 			const std::vector<char>, const Direction);
 
 private:
-	std::vector<std::vector<Board::Tile>> _board;
+	Board::Tile _board[HEIGHT_BOARD][WIDTH_BOARD];
 };
--- common/src/Board.cpp
+++ common/src/Board.cpp
@@ -7,17 +7,6 @@
 
 #include <iostream>
 #include "Board.hpp"
-#include "Utils.hpp"
-
-Board::Board()
-{
-	std::vector<Tile>  lines;
-
-	lines.resize(WIDTH_BOARD);
-	for (unsigned int i = 0; i < HEIGHT_BOARD; i++) {
-		_board.push_back(lines);
-	}
-}
 
 void Board::Tile::setSprites(const std::vector<std::string> sprites)
 {
@@ -32,6 +21,16 @@ void Board::Tile::setCharacters(const std::vector<char> characteres)
 void Board::Tile::setDirection(const Direction direction)
 {
 	_direction = direction;
+}
+
+void Board::Tile::setColorBackground(const Color color)
+{
+	_background = color;
+}
+
+void Board::Tile::setColorForeground(const Color color)
+{
+	_foreground = color;
 }
 
 std::vector<std::string> Board::Tile::getSprites() const
@@ -49,6 +48,16 @@ Direction Board::Tile::getDirection() const
 	return _direction;
 }
 
+Color Board::Tile::getColorBackground() const
+{
+	return _background;
+}
+
+Color Board::Tile::getColorForeground() const
+{
+	return _foreground;
+}
+
 void Board::setSprites(const Position &pos, const std::vector<std::string> sprites)
 {
 	_board[pos.first][pos.second].setSprites(sprites);
@@ -64,6 +73,16 @@ void Board::setDirection(const Position &pos, const Direction direction)
 	_board[pos.first][pos.second].setDirection(direction);
 }
 
+void Board::setColorBackground(const Position &pos, const Color color)
+{
+	return _board[pos.first][pos.second].setColorBackground(color);
+}
+
+void Board::setColorForeground(const Position &pos, const Color color)
+{
+	return _board[pos.first][pos.second].setColorForeground(color);
+}
+
 std::vector<std::string> Board::getSprites(const Position &pos) const
 {
 	return _board[pos.first][pos.second].getSprites();
@@ -77,6 +96,16 @@ std::vector<char> Board::getCharacters(const Position &pos) const
 Direction Board::getDirection(const Position &pos) const
 {
 	return _board[pos.first][pos.second].getDirection();
+}
+
+Color Board::getColorBackground(const Position &pos) const
+{
+	return _board[pos.first][pos.second].getColorBackground();
+}
+
+Color Board::getColorForeground(const Position &pos) const
+{
+	return _board[pos.first][pos.second].getColorForeground();
 }
 
 void Board::createTile(const Position &pos, const std::vector<std::string> sprites,
