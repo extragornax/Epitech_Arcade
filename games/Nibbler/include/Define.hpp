fcfda29f67258707ca3271c214e9e295597f9dc9
arthur.sardo <arthur.sardo@epitech.eu>
2018-04-03 16:49:37 +0200

    relative path Define.hpp

--- games/Nibbler/include/Define.hpp
+++ games/Nibbler/include/Define.hpp
@@ -8,7 +8,7 @@
 #ifndef DEFINE_HPP_
 # define DEFINE_HPP_
 
-# define NIBBLER_CONF	"/home/ragnar/Delivery/OOP/cpp_arcade/games/Nibbler/misc/nibbler_conf.txt"
+# define NIBBLER_CONF	"games/Nibbler/misc/nibbler_conf.txt"
 
 // Defines for chars to use in terminal graphical libs
 # define WALL_CHAR	'#'
@@ -18,9 +18,9 @@
 
 
 // Defines for sprites to use in graphical libs
-# define WALL_SPRITE	"/home/ragnar/Delivery/OOP/cpp_arcade/games/Nibbler/misc/sprites/walls.png"
-# define SNAKE_BODY_SPRITE	"/home/ragnar/Delivery/OOP/cpp_arcade/games/Nibbler/misc/sprites/snake.png"
-# define BACKGROUND_SPRITE	"/home/ragnar/Delivery/OOP/cpp_arcade/games/Nibbler/misc/sprites/background.png"
-# define FOOD_SPRITE	"/home/ragnar/Delivery/OOP/cpp_arcade/games/Nibbler/misc/sprites/food.png"
+# define WALL_SPRITE	"games/Nibbler/misc/sprites/walls.png"
+# define SNAKE_BODY_SPRITE	"games/Nibbler/misc/sprites/snake.png"
+# define BACKGROUND_SPRITE	"games/Nibbler/misc/sprites/background.png"
+# define FOOD_SPRITE	"games/Nibbler/misc/sprites/food.png"
 
 #endif /* DEFINE_HPP_ */
